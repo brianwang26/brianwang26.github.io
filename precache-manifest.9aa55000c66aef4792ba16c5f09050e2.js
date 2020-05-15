d313127a2b54951d227d3bb1c9aa5bba8a0418e2
brianwang26 <brian.c.wang.23@dartmouth.edu>
2020-05-15 05:07:25 -0700

    Updates

--- precache-manifest.9aa55000c66aef4792ba16c5f09050e2.js
+++ precache-manifest.9aa55000c66aef4792ba16c5f09050e2.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7916059c18076653a53eacf143572026",
+    "revision": "82ae4923276559580bc6545108b36d7e",
     "url": "/index.html"
   },
   {
-    "revision": "3d098866717b5f10106f",
+    "revision": "31f71720b39f8894dda5",
     "url": "/static/css/2.a641e29a.chunk.css"
   },
   {
-    "revision": "ee9a99cbb5393b67f626",
-    "url": "/static/css/main.a0373ce0.chunk.css"
+    "revision": "fcb1a24d5a2e8055463d",
+    "url": "/static/css/main.c96e85a8.chunk.css"
   },
   {
-    "revision": "3d098866717b5f10106f",
-    "url": "/static/js/2.14d00545.chunk.js"
+    "revision": "31f71720b39f8894dda5",
+    "url": "/static/js/2.69c27141.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/static/js/2.14d00545.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.69c27141.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ee9a99cbb5393b67f626",
-    "url": "/static/js/main.d7475edb.chunk.js"
+    "revision": "fcb1a24d5a2e8055463d",
+    "url": "/static/js/main.0aff94d3.chunk.js"
   },
   {
     "revision": "a896469b35ab17db43f7",
@@ -36,11 +36,19 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/basketball.b4b72835.png"
   },
   {
+    "revision": "0667dd29386607dd06f9ecaf0ec416a4",
+    "url": "/static/media/brothers.0667dd29.png"
+  },
+  {
     "revision": "48b4bcf7684a4de093ef1421b2d3cb2f",
     "url": "/static/media/kayak.48b4bcf7.jpeg"
   },
   {
     "revision": "74d11b59f1c2f5ec471b243dcbac1089",
     "url": "/static/media/selfImage.74d11b59.png"
+  },
+  {
+    "revision": "35060ab2baf21dc928aa02171c16a389",
+    "url": "/static/media/umbrella.35060ab2.png"
   }
 ]);
