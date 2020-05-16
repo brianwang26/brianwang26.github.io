8dc6987eef7a31cecb2e29eef1db37eaa1dfc1a5
brianwang26 <brian.c.wang.23@dartmouth.edu>
2020-05-16 01:14:05 -0700

    adding screenshot to repo

--- build/precache-manifest.21dbbaa2c57ae756c82f887b4e939fb9.js
+++ build/precache-manifest.21dbbaa2c57ae756c82f887b4e939fb9.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "843b56e252897e14651e7f4a19da3442",
+    "revision": "45b73acc1a1f20fb550f070a7fc017e6",
     "url": "/index.html"
   },
   {
-    "revision": "028eed84fa5b49075714",
+    "revision": "7c4c8d3b0efc09fc9546",
     "url": "/static/css/2.a641e29a.chunk.css"
   },
   {
-    "revision": "4aa16e878f3027dd99ac",
-    "url": "/static/css/main.cf37e699.chunk.css"
+    "revision": "fcda6ed66f4dca07b9a7",
+    "url": "/static/css/main.8e3fa5db.chunk.css"
   },
   {
-    "revision": "028eed84fa5b49075714",
-    "url": "/static/js/2.70ca697f.chunk.js"
+    "revision": "7c4c8d3b0efc09fc9546",
+    "url": "/static/js/2.91019b3c.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/static/js/2.70ca697f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.91019b3c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4aa16e878f3027dd99ac",
-    "url": "/static/js/main.cde78b81.chunk.js"
+    "revision": "fcda6ed66f4dca07b9a7",
+    "url": "/static/js/main.c447962a.chunk.js"
   },
   {
     "revision": "a896469b35ab17db43f7",
@@ -32,6 +32,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/Football.a7fe9944.png"
   },
   {
+    "revision": "48cd1076577c1acdbb806db97f9db542",
+    "url": "/static/media/Gym.48cd1076.png"
+  },
+  {
     "revision": "b4b7283523677e2bdc0fb1a49d493411",
     "url": "/static/media/basketball.b4b72835.png"
   },
@@ -40,12 +44,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/brothers.0667dd29.png"
   },
   {
+    "revision": "51a95b600353e78e5d3d2e6bbcf5aa86",
+    "url": "/static/media/camping.51a95b60.png"
+  },
+  {
     "revision": "25992006d9dfd86fee1e8f40ebc9010c",
     "url": "/static/media/deer.25992006.png"
   },
   {
     "revision": "48b4bcf7684a4de093ef1421b2d3cb2f",
     "url": "/static/media/kayak.48b4bcf7.jpeg"
+  },
+  {
+    "revision": "6838384f6abbfe536023f34adf24d76f",
+    "url": "/static/media/littletikes.6838384f.png"
   },
   {
     "revision": "74d11b59f1c2f5ec471b243dcbac1089",
@@ -58,5 +70,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "35060ab2baf21dc928aa02171c16a389",
     "url": "/static/media/umbrella.35060ab2.png"
+  },
+  {
+    "revision": "0cf8f74e79253c27ac8644e2fa054f21",
+    "url": "/static/media/youngBrian.0cf8f74e.png"
   }
 ]);
